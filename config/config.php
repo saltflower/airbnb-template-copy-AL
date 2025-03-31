9a55f621f45490d294dfa8056f63115c85fff9a1
saltflower <132307660+saltflower@users.noreply.github.com>
2025-03-31 23:29:51 +0000

    Initial commit

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+
+define("SERVER", "myServerName");
+define("PORT", "myPort");
+define("USERNAME", "mysqlUser");
+define("PASSWORD", "mysqlPassword");
+define("DATABASE", "dbName");
+
+
+?>
