01098af033351c3e7564c5086fddb34fb6ebf09e
saltflower <132307660+saltflower@users.noreply.github.com>
2025-04-02 22:20:14 +0000

    Hello

--- src/functions.php
+++ src/functions.php
@@ -22,6 +22,67 @@ function dbConnect(){
     return $db;
 }
 
+function initialSql($n, $r, $g) {
+            $s = "SELECT l.id, n.neighborhood, r.type, l.accommodates, l.pictureUrl, l.name, l.rating, l.price FROM listings l
+            LEFT JOIN neighborhoods n ON l.neighborhoodId = n.id
+            LEFT JOIN roomTypes r ON l.roomTypeId = r.id ";
+            if ($n == "Any") {
+                $s .= "WHERE ";
+            } else {
+                $s .= "WHERE l.neighborhoodId = $n AND ";
+            }
+            if ($r == "Any") {
+                $s .= "l.roomTypeId >= 1";
+            } else {
+                $s .= " l.roomTypeId = $r";
+            }
+            $s .= " AND l.accommodates = $g LIMIT 20;";
+       
+            return $s;
+        }
 
+        function countSql($n, $r, $g) {
+            $s = "SELECT COUNT(*) AS total_count FROM (
+                SELECT 1 FROM listings WHERE ";
+            if ($n == "Any") {
+                $s .= "1 ";
+            } else {
+                $s .= "neighborhoodId = $n ";
+            }
+            if ($r == "Any") {
+                $s .= "AND roomTypeId >= 1 ";
+            } else {
+                $s .= "AND roomTypeId = $r ";
+            }
+            $s .= "AND accommodates = $g LIMIT 20) AS subquery;";
+            
+            return $s;
+        }
+
+        function neighborhoodTrans($n) {
+            if ($n == "Any") {
+                return $n;
+            } else {
+                $s = "SELECT neighborhood FROM neighborhoods WHERE id = $n";
+                $conn = dbConnect();
+                foreach ($conn->query($s) as $row) {
+                    return $row['neighborhood'];
+                }
+            }
+    
+        }
+
+        function roomTrans($r) {
+            if ($r == "Any") {
+                return $r;
+            } else {
+                $s = "SELECT type FROM roomTypes WHERE id = $r";
+                $conn = dbConnect();
+                foreach ($conn->query($s) as $row) {
+                    return $row['type'];
+                }
+            }
+    
+        }
 
 ?>
